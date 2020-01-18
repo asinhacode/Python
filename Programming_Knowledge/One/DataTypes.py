dc982c9bc69e536de87ebd6c5c78f188a32e0f39
Anupam Sinha <57113053+asinhacode@users.noreply.github.com>
2020-01-18 17:40:10 -0500

    floor function

---
+++ Programming_Knowledge/One/DataTypes.py
@@ -0,0 +1,11 @@
+# Please Excuse My Dear Aunt Sally
+# exponent
+for i in range(10):
+    print(2 ** i)
+
+print()
+# floor division
+print("Integer return:")
+print(18 // 4)
+print("Real return:")
+print(18 / 4)
