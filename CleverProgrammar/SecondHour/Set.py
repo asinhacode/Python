7a87a935b31b87750bd52b54200ae2bbc8494679
Anupam Sinha <57113053+asinhacode@users.noreply.github.com>
2020-01-17 19:17:55 -0500

    Add files via upload

---
+++ CleverProgrammar/SecondHour/Set.py
@@ -0,0 +1,13 @@
+vowels = {'aeiou'}
+print(vowels)
+print(len(vowels))
+
+vowels1 = {'a', 'e', 'i', 'o', 'u'}
+print(vowels1)
+print(len(vowels1))
+
+print('z' in vowels1)
+print('a' in vowels1)
+
+vowels1.add('h')
+print(vowels1)
