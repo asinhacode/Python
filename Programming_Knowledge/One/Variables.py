c2cd21bb2bdaef990da48f91098c2bea27ce0cad
Anupam Sinha <57113053+asinhacode@users.noreply.github.com>
2020-01-19 08:07:16 -0500

    Look variables, I can change too.

---
+++ Programming_Knowledge/One/Variables.py
@@ -0,0 +1,20 @@
+myAge = 1E1  # 1000
+print(myAge)
+
+myName = "Sin_ner"
+print(myName)
+
+# conversion of int to string
+print(float(myAge * 2))
+
+# check type of value
+print(type(myAge))
+print(type(myName))
+
+# multiple arguments within print statement, separated by commas
+print("My Name: ", myName, " and I am ", myAge, " years old.")
+
+# accessing different methods of string type
+print(myName.capitalize())
+print(myName.find("n"))
+
